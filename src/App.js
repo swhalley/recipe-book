b5e596755a828ef9376e50c19d72eeb69eea5b03
Sean Whalley <swhalley@gmail.com>
2018-05-03 08:54:57 -0300

    Ability to save a recipe

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import MainMenu from './components/menu/MainMenu';
+import firebase from 'firebase';
 
 class App extends Component {
   render() {
@@ -8,6 +9,15 @@ class App extends Component {
         <MainMenu />
       </div>
     );
+  }
+
+  componentDidMount(){
+    var config = {
+      apiKey: "AIzaSyDUTdna7AN7hMLIS2jPPZVLKIxj4rURduU",
+      authDomain: "recipe-book-prep.firebaseapp.com",
+      databaseURL: "https://recipe-book-prep.firebaseio.com"
+    };
+    firebase.initializeApp(config);
   }
 }
 
--- src/repository/recipeRepository.js
+++ src/repository/recipeRepository.js
@@ -1,8 +1,15 @@
-
+import firebase from 'firebase';
+import moment from 'moment-timezone';
+import uuidv4 from 'uuid/v4';
 
 export default {
     create( {title, summary, method, image } ){
-        console.log( title, summary, method, image )
+        firebase.database().ref( 'recipe/' + uuidv4() ).set({
+            title, summary, method,
+            image : image,
+            createdDate :  moment().tz('America/New_York').format(),
+            createdBy: firebase.auth().currentUser ? firebase.auth().currentUser.uid : "Testing" 
+        });
     }
 }
 
